ae32385efabb99616d15a5df650faad4f5013dfd
Diao <huangjiandiao@chuangjia.me>
2018-04-30 00:36:03 +0800

    add

---
+++ NavigationBarTransition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigationBarTransition
+//
+//  Created by Diao on 2018/4/30.
+//  Copyright © 2018年 Diao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
