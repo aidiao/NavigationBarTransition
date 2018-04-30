9c31936dd81191510e4aac9cfe7927cddf169a1d
Diao <huangjiandiao@chuangjia.me>
2018-04-30 11:49:09 +0800

    0.0.1-beta3

--- Example/NavigationBarTransition/ViewController.swift
+++ Example/NavigationBarTransition/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import NavigationBarTransition
 
 class ViewController: UIViewController {
 
---
+++ NavigationBarTransition/Classes/NavigationController.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationController.swift
+//  NavigationBarTransition
+//
+//  Created by Diao on 2018/4/30.
+//
+
+import UIKit
+
+public class NavigationController: UINavigationController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
